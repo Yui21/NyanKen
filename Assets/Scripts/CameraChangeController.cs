4a7a8510a00813ea3836abd205f4f7f24c12bef5
Yui21 <84171257+Yui21@users.noreply.github.com>
2023-10-09 12:37:24 +0900

    ボタンで視点切り替え

--- Assets/Scripts/CameraChangeController.cs
+++ Assets/Scripts/CameraChangeController.cs
@@ -43,42 +43,77 @@ public class CameraChangeController : MonoBehaviour
     }
 
     
-    void Update()
-    {
-        if(Input.GetKey(KeyCode.C))//「C」を押してる間はFPSになる
-        {
-            //FPSになる
-            catCameraRoot.SetActive(true);
-            catEyeCamera.SetActive(true);
-            uICanvasFps.SetActive(true);
-            cuCat.GetComponent<FirstPersonController>().enabled = true;
+    // void Update()
+    // {
+    //     if(Input.GetKey(KeyCode.C))//「C」を押してる間はFPSになる
+    //     {
+    //         //FPSになる
+    //         catCameraRoot.SetActive(true);
+    //         catEyeCamera.SetActive(true);
+    //         uICanvasFps.SetActive(true);
+    //         cuCat.GetComponent<FirstPersonController>().enabled = true;
 
-            //テストで試した。これは実行できた
-            cuCat.GetComponent<Test>().enabled = true;
+    //         //テストで試した。これは実行できた
+    //         cuCat.GetComponent<Test>().enabled = true;
         
 
-            playerCameraRoot.SetActive(false);
-            mainCamera.SetActive(false);
-            playerFollowCamera.SetActive(false);
-            uICanvasTps.SetActive(false);
-            cuCat.GetComponent<ThirdPersonController>().enabled = false;
+    //         playerCameraRoot.SetActive(false);
+    //         mainCamera.SetActive(false);
+    //         playerFollowCamera.SetActive(false);
+    //         uICanvasTps.SetActive(false);
+    //         cuCat.GetComponent<ThirdPersonController>().enabled = false;
             
 
-        }
-        else
-        {
-            //TPSに戻る
-            playerCameraRoot.SetActive(true);
-            mainCamera.SetActive(true);
-            playerFollowCamera.SetActive(true);
-            uICanvasTps.SetActive(true);
-            cuCat.GetComponent<ThirdPersonController>().enabled = true;
+    //     }
+    //     else
+    //     {
+    //         //TPSに戻る
+    //         playerCameraRoot.SetActive(true);
+    //         mainCamera.SetActive(true);
+    //         playerFollowCamera.SetActive(true);
+    //         uICanvasTps.SetActive(true);
+    //         cuCat.GetComponent<ThirdPersonController>().enabled = true;
 
-            catCameraRoot.SetActive(false);
-            catEyeCamera.SetActive(false);
-            uICanvasFps.SetActive(false);
-            cuCat.GetComponent<FirstPersonController>().enabled = false;
+    //         catCameraRoot.SetActive(false);
+    //         catEyeCamera.SetActive(false);
+    //         uICanvasFps.SetActive(false);
+    //         cuCat.GetComponent<FirstPersonController>().enabled = false;
 
-        }
+    //     }
+    // }
+
+
+    public void toCatCamera()  //FPSになる
+    {
+        catCameraRoot.SetActive(true);
+        catEyeCamera.SetActive(true);
+        uICanvasFps.SetActive(true);
+        cuCat.GetComponent<FirstPersonController>().enabled = true;
+
+        //テストで試した。これは実行できた
+        cuCat.GetComponent<Test>().enabled = true;
+        
+
+        playerCameraRoot.SetActive(false);
+        mainCamera.SetActive(false);
+        playerFollowCamera.SetActive(false);
+        uICanvasTps.SetActive(false);
+        cuCat.GetComponent<ThirdPersonController>().enabled = false;
+
+    }
+
+    public void toDroneCamera()  //TPSになる
+    {
+        playerCameraRoot.SetActive(true);
+        mainCamera.SetActive(true);
+        playerFollowCamera.SetActive(true);
+        uICanvasTps.SetActive(true);
+        cuCat.GetComponent<ThirdPersonController>().enabled = true;
+
+        catCameraRoot.SetActive(false);
+        catEyeCamera.SetActive(false);
+        uICanvasFps.SetActive(false);
+        cuCat.GetComponent<FirstPersonController>().enabled = false;
+
     }
 }
