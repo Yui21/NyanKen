37cde84ad608c3c8a886b81dfbe4c2d7bfca0348
Yui21 <84171257+Yui21@users.noreply.github.com>
2023-10-14 22:04:22 +0900

    1014

--- Assets/Scripts/WorkRoomAction.cs
+++ Assets/Scripts/WorkRoomAction.cs
@@ -4,15 +4,125 @@ using UnityEngine;
 
 public class WorkRoomAction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //public GameObject uIPillowAction;
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerStay(Collider other)
     {
         
+        if (other.CompareTag("Player"))
+        {
+            if(gameObject.name == "airCon_Con")
+            {
+                Debug.Log("エアコンのリモコンと衝突した！！！！");
+                //uIPillowAction.SetActive(true);
+            }
+
+            if(gameObject.name == "comicBook")
+            {
+                Debug.Log("漫画の山と衝突した！！！！");
+                //uIPillowAction.SetActive(true);
+            }
+            if(gameObject.name == "cup")
+            {
+                Debug.Log("コップと衝突した！！！！");
+                //uIPillowAction.SetActive(true);
+            }
+            
+            if(gameObject.name == "garbageCan")
+            {
+                Debug.Log("ゴミ箱と衝突した！！！！");
+                //uIPillowAction.SetActive(true);
+            }
+            if(gameObject.name == "pillow")
+            {
+                Debug.Log("枕と衝突した！！！！");
+                //uIPillowAction.SetActive(true);
+            }
+            
+            if(gameObject.name == "PC")
+            {
+                Debug.Log("PCと衝突した！！！！");
+                //uIPillowAction.SetActive(true);
+            }
+
+            if(gameObject.name == "backRest")
+            {
+                Debug.Log("いすと衝突した！！！！");
+                //uIPillowAction.SetActive(true);
+            }
+
+            if(gameObject.name == "shelf")
+            {
+                Debug.Log("緑の棚と衝突した！！！！");
+                //uIPillowAction.SetActive(true);
+            }
+
+            if(gameObject.name == "oshiire")
+            {
+                Debug.Log("押入れと衝突した！！！！");
+                //uIPillowAction.SetActive(true);
+            }
+            
+
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if(gameObject.name == "airCon_Con")
+            {
+                Debug.Log("エアコンのリモコンから離れた！！！！");
+                //uIPillowAction.SetActive(true);
+            }
+
+            if(gameObject.name == "comicBook")
+            {
+                Debug.Log("漫画の山から離れた！！！！");
+                //uIPillowAction.SetActive(true);
+            }
+
+            if(gameObject.name == "cup")
+            {
+                Debug.Log("コップから離れた！！！！");
+                //uIPillowAction.SetActive(true);
+            }
+            
+            if(gameObject.name == "garbageCan")
+            {
+                Debug.Log("ゴミ箱から離れた！！！！");
+                //uIPillowAction.SetActive(true);
+            }
+
+            if(gameObject.name == "pillow")
+            {
+                Debug.Log("枕から離れた！！！！");
+                //uIPillowAction.SetActive(true);
+            }
+            
+            if(gameObject.name == "PC")
+            {
+                Debug.Log("PCから離れた！！！！");
+                //uIPillowAction.SetActive(true);
+            }
+
+            if(gameObject.name == "backRest")
+            {
+                Debug.Log("いすから離れた！！！！");
+                //uIPillowAction.SetActive(true);
+            }
+
+            if(gameObject.name == "shelf")
+            {
+                Debug.Log("緑の棚から離れた！！！！");
+                //uIPillowAction.SetActive(true);
+            }
+
+            if(gameObject.name == "oshiire")
+            {
+                Debug.Log("押入れから離れた！！！！");
+                //uIPillowAction.SetActive(true);
+            }
+        }
     }
 }
