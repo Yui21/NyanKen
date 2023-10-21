37378c435058857dd0a52d5717304e82f59fd3c5
Yui21 <84171257+Yui21@users.noreply.github.com>
2023-10-21 11:52:37 +0900

    クリアパネル

--- Assets/Scripts/CountDownTimer.cs
+++ Assets/Scripts/CountDownTimer.cs
@@ -16,18 +16,26 @@ public class CountDownTimer : MonoBehaviour
 	//　前回Update時の秒数
 	private float oldSeconds;
 	private Text timerText;
+
+
+    //クリアパネル
+    public GameObject clearPanel;
  
-	void Start () {
+	void Start () 
+    {
 		totalTime = minute * 60 + seconds;
 		oldSeconds = 0f;
 		timerText = GetComponentInChildren<Text>();
 	}
  
-	void Update () {
+	void Update () 
+    {
 		//　制限時間が0秒以下なら何もしない
-		if (totalTime <= 0f) {
+		if (totalTime <= 0f) 
+        {
 			return;
 		}
+
 		//　一旦トータルの制限時間を計測；
 		totalTime = minute * 60 + seconds;
 		totalTime -= Time.deltaTime;
@@ -37,13 +45,20 @@ public class CountDownTimer : MonoBehaviour
 		seconds = totalTime - minute * 60;
  
 		//　タイマー表示用UIテキストに時間を表示する
-		if((int)seconds != (int)oldSeconds) {
+		if((int)seconds != (int)oldSeconds) 
+        {
 			timerText.text = minute.ToString("00") + ":" + ((int) seconds).ToString("00");
 		}
+
 		oldSeconds = seconds;
+
 		//　制限時間以下になったらコンソールに『制限時間終了』という文字列を表示する
-		if(totalTime <= 0f) {
+		if(totalTime <= 0f) 
+        {
 			Debug.Log("制限時間終了");
+            //クリアパネルを表示
+            clearPanel.SetActive(true);
+
 		}
 	}
 }
